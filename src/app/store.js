4058cdd478b7f0ba390a5854f104b26d70eeb5a8
Dylan Gee <dylangee@gmail.com>
2022-09-09 21:21:24 +0100

    Subreddit Reducer added to store. Default store deleted

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import subredditReducer from '../features/subreddit/subredditSlice.js';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    subreddits: subredditReducer
   },
 });
