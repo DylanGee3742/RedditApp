d01f1f1c8d24132f440e6fac8b18608dbe9fc251
Dylan Gee <dylangee@gmail.com>
2022-09-12 22:33:10 +0100

    Post reducer added

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import subredditReducer from '../features/subreddit/subredditSlice.js';
+import postsReducer from '../features/posts/postsSlice';
 
 export const store = configureStore({
   reducer: {
-    subreddits: subredditReducer
+    subreddits: subredditReducer,
+    posts: postsReducer
   },
 });
