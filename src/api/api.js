d7182ab22ca9948fc9ddf10f51bc60588c4fa539
Dylan Gee <dylangee@gmail.com>
2022-09-12 22:33:44 +0100

    Call to api to fetch posts. Works fine, retrieved correct data

--- src/api/api.js
+++ src/api/api.js
@@ -2,12 +2,26 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 
 export const ROOT_API = `https://www.reddit.com/`;
 
+// Fetch Subreddits
 export const fetchSubreddits = createAsyncThunk(
     'subreddit/fetchSubreddit',
     async () => {
-        const response = await fetch(`https://www.reddit.com/subreddits.json`)
+        const response = await fetch(`${ROOT_API}subreddits.json`)
         const json = await response.json()
         return json.data.children.map(subreddit => subreddit.data);
     }
 )
+
+// Fetch Posts
+export const fetchPosts = createAsyncThunk(
+    'posts/fetchPosts',
+    async () => {
+        const response = await fetch(`${ROOT_API}r/AskReddit/.json`)
+        const json = await response.json()
+        return json.data.children.map(post => post.data);
+    }
+)
+
+
+// Fetch Comments
 
