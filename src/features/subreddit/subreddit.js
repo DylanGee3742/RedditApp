f53b5bfe3322a91224e25cab6e9d9a800b8fd9e3
Dylan Gee <dylangee@gmail.com>
2022-09-12 21:50:39 +0100

    Mapping of subreddits mostly complete. Needs extra work once posts section complete

--- src/features/subreddit/subreddit.js
+++ src/features/subreddit/subreddit.js
@@ -5,21 +5,29 @@ import { fetchSubreddits} from "./subredditSlice";
 
 
 export const Subreddits = () => {
-    const { loaded, setLoaded } = useState(false)
     const subreddits = useSelector(selectSubreddits);
     const dispatch = useDispatch()
 
     useEffect(() => {
         dispatch(fetchSubreddits())
-    }, [])
+    }, []);
 
     return (
         <div className = "subreddits">
                 <h1>Subreddits</h1>
-                {subreddits.map((subreddit, i) => {
-                    return subreddit.subreddit
-                })}
-            
+                {subreddits.map((subreddit) => (
+                    <li key={subreddit.id}>
+                        {/* Once posts section sorted, link button click to return posts of that sub not reassing window */}
+                        <button onClick={() => window.location.assign(`https://www.reddit.com${subreddit.url}`) }>
+                            <img src = {subreddit.icon_img}
+                            alt = "Subreddit Icon"
+                            className = "subreddit_icon"
+                            style = {{ height: '2em'}}
+                            />
+                            {subreddit.display_name}
+                        </button>
+                    </li>
+                ))}
         </div>
     );
 }
