88edc491899cdc1fd5f790518289b997d30ab1ee
Dylan Gee <dylangee@gmail.com>
2022-09-13 22:21:55 +0100

    Async thunk changed so takes selected subreddit and fetchs posts from it

--- src/api/api.js
+++ src/api/api.js
@@ -15,8 +15,8 @@ export const fetchSubreddits = createAsyncThunk(
 // Fetch Posts
 export const fetchPosts = createAsyncThunk(
     'posts/fetchPosts',
-    async () => {
-        const response = await fetch(`${ROOT_API}r/AskReddit/.json`)
+    async (selected) => {
+        const response = await fetch(`${ROOT_API}${selected}.json`)
         const json = await response.json()
         return json.data.children.map(post => post.data);
     }
