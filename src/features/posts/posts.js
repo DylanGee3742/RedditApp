adbe159a147838d18e4cf2e2755a6e99384f71aa
Dylan Gee <dylangee@gmail.com>
2022-09-13 22:20:48 +0100

    Some info from posts from subreddit are rendered to page

--- src/features/posts/posts.js
+++ src/features/posts/posts.js
@@ -1,20 +1,30 @@
-import { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { fetchPosts } from "../../api/api";
+import { useSelector } from "react-redux";
 import { selectPosts } from "./postsSlice";
+import moment from 'moment';
+import "./post.css";
 
 
 export const Posts = () => {
     const posts = useSelector(selectPosts);
-    const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(fetchPosts())
-    }, []);
 
     return (
         <div>
-            <h1>Posts</h1>
+            {posts.map((post) => (
+                <li 
+                key={post.id}
+                className= 'post'>
+                    <span>Posted By {post.author}</span>
+                    <span>  </span>
+                    <span>{moment.unix(post.created_utc).fromNow()}</span>
+                    <br></br>
+                    <span>{post.title}</span>
+                    <br></br>
+                    <span>{ post.is_video ? <video height = '150' width = '300' controls >
+                        <source src = {post.media.reddit_video.fallback_url} type="video/mp4" className = 'post_media' /> </video>
+                     : < img src = {post.thumbnail} className = 'post_media' /> }</span>
+                </li>
+            ))}
         </div>
     )
 }
