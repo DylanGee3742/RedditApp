1fe34ca2c7a243e13457c86d722f8e7675e22cfc
Dylan Gee <dylangee@gmail.com>
2022-09-13 22:20:07 +0100

    dispatch to get posts added. OnClick on subreddit sets selected state to that subreddit

--- src/features/subreddit/subreddit.js
+++ src/features/subreddit/subreddit.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { fetchSubreddits } from "../../api/api";
+import { fetchPosts, fetchSubreddits } from "../../api/api";
 import { selectSubreddits } from "./subredditSlice";
 
+
 export const Subreddits = () => {
+    const [ selected, setSelected ] = useState('r/popular')
     const subreddits = useSelector(selectSubreddits);
     const dispatch = useDispatch()
 
@@ -11,13 +13,16 @@ export const Subreddits = () => {
         dispatch(fetchSubreddits())
     }, []);
 
+    useEffect(() => {
+        dispatch(fetchPosts(selected));
+    }, []);
+
     return (
         <div className = "subreddits">
                 <h1>Subreddits</h1>
                 {subreddits.map((subreddit) => (
                     <li key={subreddit.id}>
-                        {/* Once posts section sorted, link button click to return posts of that sub not reassing window */}
-                        <button onClick={() => window.location.assign(`https://www.reddit.com${subreddit.url}`) }>
+                        <button onClick={() => setSelected(subreddit.url) }>
                             <img src = {subreddit.icon_img}
                             alt = "Subreddit Icon"
                             className = "subreddit_icon"
