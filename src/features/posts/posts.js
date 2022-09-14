297afb476daac28ee3f2400e4a1c3a01fac7320c
Dylan Gee <dylangee@gmail.com>
2022-09-14 22:52:23 +0100

    Post changed to render article instead of list as formatting works better

--- src/features/posts/posts.js
+++ src/features/posts/posts.js
@@ -1,30 +1,29 @@
 import { useSelector } from "react-redux";
 import { selectPosts } from "./postsSlice";
+import './posts';
 import moment from 'moment';
-import "./post.css";
 
 
 export const Posts = () => {
     const posts = useSelector(selectPosts);
 
-
     return (
-        <div>
+        <article className = "post-container">
+            <h1>Posts</h1>
+
             {posts.map((post) => (
-                <li 
+                <article 
                 key={post.id}
-                className= 'post'>
-                    <span>Posted By {post.author}</span>
-                    <span>  </span>
-                    <span>{moment.unix(post.created_utc).fromNow()}</span>
-                    <br></br>
-                    <span>{post.title}</span>
-                    <br></br>
+                className= 'indiv-post'>
+                    <h4>{moment.unix(post.created_utc).fromNow()}
+                    <span> | </span>{post.author}
+                    <span> | </span>{post.title}</h4>
                     <span>{ post.is_video ? <video height = '150' width = '300' controls >
                         <source src = {post.media.reddit_video.fallback_url} type="video/mp4" className = 'post_media' /> </video>
                      : < img src = {post.thumbnail} className = 'post_media' /> }</span>
-                </li>
+                </article>               
             ))}
-        </div>
+
+        </article>
     )
 }
