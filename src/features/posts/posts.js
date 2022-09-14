3544bd99df65b97e80d98ff1677a6fbc8df78c9c
Dylan Gee <dylangee@gmail.com>
2022-09-14 23:33:40 +0100

    import name change

--- src/features/posts/posts.js
+++ src/features/posts/posts.js
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
 import { selectPosts } from "./postsSlice";
-import './posts';
+import './posts.css';
 import moment from 'moment';
 
 
@@ -8,22 +8,22 @@ export const Posts = () => {
     const posts = useSelector(selectPosts);
 
     return (
-        <article className = "post-container">
+        <div className = "post-container" >
             <h1>Posts</h1>
 
             {posts.map((post) => (
                 <article 
                 key={post.id}
-                className= 'indiv-post'>
+                className= 'singlepost'>
                     <h4>{moment.unix(post.created_utc).fromNow()}
                     <span> | </span>{post.author}
                     <span> | </span>{post.title}</h4>
                     <span>{ post.is_video ? <video height = '150' width = '300' controls >
                         <source src = {post.media.reddit_video.fallback_url} type="video/mp4" className = 'post_media' /> </video>
-                     : < img src = {post.thumbnail} className = 'post_media' /> }</span>
+                     : < img src = {post.thumbnail || null } className = 'post_media' /> }</span>
                 </article>               
             ))}
 
-        </article>
+        </div>
     )
 }
