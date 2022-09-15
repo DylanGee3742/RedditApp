b096587186e4c7f5390531f03d2c6e358d647ca7
Dylan Gee <dylangee@gmail.com>
2022-09-15 09:51:50 +0100

    button for up/down added. displaying media updated

--- src/features/posts/posts.js
+++ src/features/posts/posts.js
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import { selectPosts } from "./postsSlice";
 import './posts.css';
 import moment from 'moment';
+import { BsFillArrowUpSquareFill, BsArrowDownSquareFill } from 'react-icons/bs';
 
 
 export const Posts = () => {
@@ -15,12 +16,18 @@ export const Posts = () => {
                 <article 
                 key={post.id}
                 className= 'singlepost'>
+                    <button><BsFillArrowUpSquareFill />{post.ups}</button>
+                    <div className='postcontent'>
                     <h4>{moment.unix(post.created_utc).fromNow()}
                     <span> | </span>{post.author}
                     <span> | </span>{post.title}</h4>
-                    <span>{ post.is_video ? <video height = '150' width = '300' controls >
+                    {/* if 'https: not included post.thumbnail */}
+                     <span>{post.thumbnail === 'self'  ? null : <span className='postmedia'>{ post.is_video ? <video height = '150' width = '300' controls >
                         <source src = {post.media.reddit_video.fallback_url} type="video/mp4" className = 'post_media' /> </video>
-                     : < img src = {post.thumbnail || null } className = 'post_media' /> }</span>
+                     : < img src = {post.thumbnail } height = '150' width = '300' className = 'post_media' /> }</span> }</span>
+                     <br></br>
+                    </div>
+                     <button><BsArrowDownSquareFill />{post.downs}</button>
                 </article>               
             ))}
 
