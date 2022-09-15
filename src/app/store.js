8e3ec3c2ee7573fcd5cfee4e1a16bd0b9c0e33cd
Dylan Gee <dylangee@gmail.com>
2022-09-15 22:05:26 +0100

    comment reducer added to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import subredditReducer from '../features/subreddit/subredditSlice.js';
 import postsReducer from '../features/posts/postsSlice';
+import commentsReducer from '../features/comments/commentsSlice';
 
 export const store = configureStore({
   reducer: {
     subreddits: subredditReducer,
-    posts: postsReducer
+    posts: postsReducer,
+    comments: commentsReducer
   },
 });
