35c3b25b5a9d83909db5ca14bbdfece4a96b0c37
Dylan Gee <dylangee@gmail.com>
2022-09-15 22:05:48 +0100

    call to api to fetch comments (still in progress)

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,5 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import { isCompositeComponent } from "react-dom/test-utils";
 
 export const ROOT_API = `https://www.reddit.com/`;
 
@@ -8,6 +9,7 @@ export const fetchSubreddits = createAsyncThunk(
     async () => {
         const response = await fetch(`${ROOT_API}subreddits.json`)
         const json = await response.json()
+
         return json.data.children.map(subreddit => subreddit.data);
     }
 )
@@ -18,10 +20,19 @@ export const fetchPosts = createAsyncThunk(
     async (selected) => {
         const response = await fetch(`${ROOT_API}${selected}.json`)
         const json = await response.json()
+
         return json.data.children.map(post => post.data);
     }
 )
 
 
 // Fetch Comments
+export const fetchComments = createAsyncThunk(
+    'comments/fetchComments', 
+    async (permalink) => {
+        const response = await fetch(`${ROOT_API}${permalink}.json`)
+        const json = await response.json()
 
+        return json[1].data.data.children.map(subreddit => subreddit.data);
+    }
+)
