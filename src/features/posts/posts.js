b2cb39935499315711f7b5f9a12088bb70a09559
Dylan Gee <dylangee@gmail.com>
2022-09-15 22:04:50 +0100

    option to have no media if not avaiable. dispatch fetchcomments using selected permalinks

--- src/features/posts/posts.js
+++ src/features/posts/posts.js
@@ -1,17 +1,26 @@
-import { useSelector } from "react-redux";
-import { selectPosts } from "./postsSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { selectPosts, selectPostPermalink } from "./postsSlice";
 import './posts.css';
 import moment from 'moment';
 import { BsFillArrowUpSquareFill, BsArrowDownSquareFill } from 'react-icons/bs';
+import { fetchComments } from "../../api/api";
+import { selectComments } from "../comments/commentsSlice";
 
 
 export const Posts = () => {
+    const dispatch = useDispatch()
     const posts = useSelector(selectPosts);
+    const permalinks = useSelector(selectPostPermalink)
+    const comments = useSelector(selectComments);
+    console.log(posts)
+  
+    useEffect(() => {
+        dispatch(fetchComments(permalinks))
+    }, []);
 
     return (
         <div className = "post-container" >
-            <h1>Posts</h1>
-
             {posts.map((post) => (
                 <article 
                 key={post.id}
@@ -21,16 +30,14 @@ export const Posts = () => {
                     <h4>{moment.unix(post.created_utc).fromNow()}
                     <span> | </span>{post.author}
                     <span> | </span>{post.title}</h4>
-                    {/* if 'https: not included post.thumbnail */}
-                     <span>{post.thumbnail === 'self'  ? null : <span className='postmedia'>{ post.is_video ? <video height = '150' width = '300' controls >
+                     <span>{post.thumbnail.includes('https')  ? <span className='postmedia'>{ post.is_video ? <video height = '150' width = '300' controls >
                         <source src = {post.media.reddit_video.fallback_url} type="video/mp4" className = 'post_media' /> </video>
-                     : < img src = {post.thumbnail } height = '150' width = '300' className = 'post_media' /> }</span> }</span>
+                     : < img src = {post.thumbnail } height = '150' width = '300' className = 'post_media' /> }</span> : null }   </span>
                      <br></br>
                     </div>
                      <button><BsArrowDownSquareFill />{post.downs}</button>
                 </article>               
             ))}
-
         </div>
     )
 }
