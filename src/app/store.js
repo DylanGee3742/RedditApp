a3b88ef508bdaa181cdf70dbfbdc403364400122
Dylan Gee <dylangee@gmail.com>
2022-09-20 22:22:13 +0100

    search term reducer added

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import subredditReducer from '../features/subreddit/subredditSlice.js';
 import postsReducer from '../features/posts/postsSlice';
 import commentsReducer from '../features/comments/commentsSlice';
+import searchTermReducer from '../features/searchTerm/searchTermSlice';
+
 
 export const store = configureStore({
   reducer: {
     subreddits: subredditReducer,
     posts: postsReducer,
-    comments: commentsReducer
+    comments: commentsReducer,
+    searchTerm: searchTermReducer
   },
 });
