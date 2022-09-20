ef59489b174a27913b01f96d2886aad178b01045
Dylan Gee <dylangee@gmail.com>
2022-09-20 22:23:50 +0100

    New search bar created that updates the state with search term and sends that search term to call to api

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,19 +1,48 @@
 import { FaRedditAlien } from "react-icons/fa";
 import { GoSearch } from "react-icons/go";
 import "./header.css"; 
+import { setSearchTerm } from "../../features/searchTerm/searchTermSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { useState, useEffect } from "react";
+import { selectSearchTerm } from "../../features/searchTerm/searchTermSlice";
+import { fetchSearchTermPosts } from "../../api/api";
 
 export const Header = () => {
+    const [searchTermLocal, setSearchTermLocal ] = useState('');
+    const searchTerm = useSelector(selectSearchTerm);
+    const dispatch = useDispatch();
+
+    const onSearchTermChange = (e) => {
+        setSearchTermLocal(e.target.value)
+    };
+
+    useEffect(() => {
+        setSearchTermLocal(searchTerm)
+    }, [searchTerm]);
+
+    const onSearchTermSubmit = (e) => {
+        e.preventDefault();
+        setSearchTermLocal(searchTermLocal);
+        dispatch(fetchSearchTermPosts(searchTermLocal))
+    }
+
     return (
         <div className="header">
                 <FaRedditAlien className="logo" />
                 <span className="title">Reddit React</span>
 
-            <div className="search">
-                <input className="searchBar"></input>
-                <span><button className="search-button"><GoSearch className="search-icon"/></button></span>
-            </div>
-
-
+            <form className='search' onSubmit={onSearchTermSubmit}>
+                <input
+                    type="text"
+                    placeholder="Search"
+                    value={searchTermLocal}
+                    onChange={onSearchTermChange}
+                    aria-label="Search posts"
+                    />
+                <button type="submit" onClick={onSearchTermSubmit} aria-label="Search">
+                    <GoSearch />
+                </button>
+            </form>
     </div>
 
     )
